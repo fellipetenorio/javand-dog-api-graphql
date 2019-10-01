a6cd80830a63878fb076a9e31e70e352505c5186
fellipetenorio <3355473ftfN>
2019-10-01 17:01:16 -0300

    unit test with unauth get

--- src/test/java/com/tenorio/javand/dogapi/dogapigraphql/DogapigraphqlApplicationTests.java
+++ src/test/java/com/tenorio/javand/dogapi/dogapigraphql/DogapigraphqlApplicationTests.java
@@ -1,16 +1,38 @@
 package com.tenorio.javand.dogapi.dogapigraphql;
 
+import com.tenorio.javand.dogapi.dogapigraphql.service.DogService;
+import com.tenorio.javand.dogapi.dogapigraphql.webservice.DogController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(DogController.class)
 public class DogapigraphqlApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	DogService dogService;
+
 	@Test
-	public void contextLoads() {
+	public void getAllDogsWithoutAuth() throws Exception {
+		mockMvc.perform(get("/dogs2"))
+				.andExpect(status().isUnauthorized());
+//				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+//				.andExpect(content().json("[]"));
+
+		verify(dogService, times(0)).findAllDogs();
 	}
 
 }
